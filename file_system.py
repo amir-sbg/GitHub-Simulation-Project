6fee56fafc812dd1170c512f723d1180d5b1be71
Amir79Naziri <naziriamirreza@gmail.com>
2021-05-25 22:35:11 +0430

    "feat: create file system 3"

--- file_system.py
+++ file_system.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import FileCodingHandler
 
 
 class User:
@@ -7,6 +8,7 @@ class User:
     def __init__(self, username, password):
         self.__username = username
         self.__password = password
+        self.__repositories = dict()
 
     def get_username(self):
         return self.__username
@@ -14,19 +16,25 @@ class User:
     def check_password(self, password):
         return self.__password == password
 
+    def get_password(self):
+        return self.__password
+
     def __eq__(self, other):
         return self.__username == other.get_username()
+
+    def add_repository(self, repository_name):
+        self.__repositories[repository_name] = set().add(self.__username)
+
 
 
 def authenticate_user(username, password):
     users = load_users()
-    user = None
-    for u in users:
-        if u.get_username() == username and u.check_password(password):
-            user = u
-            break
 
-    return user
+    for user in users:
+        if user.get_username() == username and user.check_password(password):
+            return user
+
+    return None
 
 
 def load_users():
@@ -78,51 +86,48 @@ def allocate_new_user(username, password):
     return True
 
 
-def pull_from_user_directory(username, password, directory_name, path, file_name=None, all_files=True):
-
+def pull_server_side(username, password, path, type_):
     user = authenticate_user(username, password)
     if user is None:
         return None
-    base_directory = user.get_username()
-    path = os.path.join('./data', base_directory, path, directory_name)
-
-    try:
-        files = list()
-        if all_files:
-            for name in os.listdir(path):
-                if os.path.isfile(os.path.join(path, name)):
-                    with open(os.path.join(path, name), 'rb') as f:
-                        files.append(f.read(-1))
-
-        else:
-            with open(os.path.join(path, file_name), 'rb') as f:
-                files.append(f.read(-1))
-
-        return files
-
-    except OSError as error:
-        print(error)
-        return None
+    path = os.path.join('./data', user.get_username(), path)
+    return FileCodingHandler.encoder(type_, path)
 
 
-def create_directory_for_user(username, password, directory_name, path=''):
-
+def push_server_side(username, password, messageBody, path):
     user = authenticate_user(username, password)
+    if user is None:
+        return None
+    path = os.path.join('./data', user.get_username(), path)
+    FileCodingHandler.decoder(messageBody, path)
+
+
+def pull_client_side(path, type_):
+    return FileCodingHandler.encoder(type_, path)
+
+
+def push_client_side(messageBody, path):
+    FileCodingHandler.decoder(messageBody, path)
+
+
+def create_repository_for_user(username, password, repository_name):
+    user = authenticate_user(username, password)
+    users = load_users()
     if user is None:
         return False
     base_directory = user.get_username()
-    path = os.path.join('./data', base_directory, path, directory_name)
+    path = os.path.join('./data', base_directory, repository_name)
 
     try:
         os.mkdir(path)
     except OSError as error:
         print(error)
+    for user_ in users:
+        if user_ == user:
+            user_.add_repository(repository_name)
+            break
+
+    save_users(users)
 
     return True
 
-
-if __name__ == '__main__':
-    print(allocate_new_user('Amir2w44', '00sdfasdf'))
-    create_directory_for_user('Amir2w44', '00sdfasdf', 'HI')
-    create_directory_for_user('Amir2w44', '00sdfasdf', 'H', 'HI')
-    print(pull_from_user_directory('Amir2w44', '00sdfasdf', 'H', 'HI', all_files=True))
